3f37a0db0efef955c0a73b4d898c8c1c5a921535
Yuke Zheng <yukezheng@college.harvard.edu>
2022-04-02 17:30:25 -0700

    update

--- config.js
+++ config.js
@@ -1,55 +1,55 @@
 "use strict";
 
 module.exports = {
-  url: "https://www.yukezheng.com",
-  pathPrefix: "",
-  title: "Yuke Zheng's Portfolio",
-  subtitle: "Student at Harvard University",
-  copyright: "",
-  disqusShortname: "",
-  postsPerPage: 5,
-  googleAnalyticsId: "UA-73379983-2",
-  useKatex: false,
-  menu: [
-    {
-      label: "Projects",
-      path: "/",
+    url: "https://www.yukezheng.com",
+    pathPrefix: "",
+    title: "Yuke Zheng's Portfolio",
+    subtitle: "Harvard University",
+    copyright: "",
+    disqusShortname: "",
+    postsPerPage: 5,
+    googleAnalyticsId: "UA-73379983-2",
+    useKatex: false,
+    menu: [
+        {
+            label: "Projects",
+            path: "/",
+        },
+        {
+            label: "Architecture",
+            path: "/pages/architecture",
+        },
+        {
+            label: "About me",
+            path: "/pages/about",
+        },
+        // {
+        //     label: "Resume",
+        //     path: "/pages/resume",
+        // },
+    ],
+    author: {
+        name: "Yuke Zheng",
+        photo: "/profile-pic.jpg",
+        bio:
+            "Recent Graduate from Harvard University\nSoftware Engineer\nA.B. in Computer Science and History of Art and Architecture",
+        contacts: {
+            email: "",
+            facebook: "",
+            telegram: "",
+            twitter: "",
+            github: "yzheng1998",
+            rss: "",
+            vkontakte: "",
+            linkedin: "yuke-zheng-9611a4151/",
+            instagram: "",
+            line: "",
+            gitlab: "",
+            weibo: "",
+            codepen: "",
+            youtube: "",
+            soundcloud: "",
+            medium: "",
+        },
     },
-    {
-      label: "Architecture",
-      path: "/pages/architecture",
-    },
-    {
-      label: "About me",
-      path: "/pages/about",
-    },
-    {
-      label: "Resume",
-      path: "/pages/resume",
-    },
-  ],
-  author: {
-    name: "Yuke Zheng",
-    photo: "/profile-pic.jpg",
-    bio:
-      "Recent Graduate at Harvard University\nA.B. in Computer Science and History of Art and Architecture\nRotational Development Program at Jane Street Capital",
-    contacts: {
-      email: "",
-      facebook: "",
-      telegram: "",
-      twitter: "",
-      github: "yzheng1998",
-      rss: "",
-      vkontakte: "",
-      linkedin: "yuke-zheng-9611a4151/",
-      instagram: "",
-      line: "",
-      gitlab: "",
-      weibo: "",
-      codepen: "",
-      youtube: "",
-      soundcloud: "",
-      medium: "",
-    },
-  },
 };
